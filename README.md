58294732dce5cd213c2eaf416d7f2bc5e82e65ee
cooperdk <564150+cooperdk@users.noreply.github.com>
2021-02-21 20:16:14 +0100

    Now modified to work both as a command-line utility and as a module.

--- README.md
+++ README.md
@@ -1,51 +1,8 @@
 # XXX Folder Flattener
 
-#### A command-line utility to flatten adult video scene directories. Companion tool for [YAPO](https://github.com/cooperdk/YAPO-e-plus) and other porn organizers
+#### A cross-platform utility to flatten adult video scene directories
 
 #### Will work from an input directory and move all files below the parent subfolders to the parent subfolder, and delete any trash files (configurable in flatten-config.json)
-
-[Get a compiled version of the utility here](https://github.com/cooperdk/XXX-Folder-Flattener/releases)
-
-The app will take a directory and flatten all subdirectories. Also, files of configurable types will be removed in the process, as well as the directories the scenes were originally placed in, if they are empty.
-
-You will have to move files/directories with scenes from your various websites/networks to their individual main directories, the utility will then flatten the directory structure within those directories.
-
-Example:
-
-
-```
-1000 Facials
-  -1000 Facials - Ella Knox Blowbang
-    -1000Facials - Ella Knox - Ella Knox Blowbang.mp4
-    -1000Facials - Ella Knox - Ella Knox Blowbang.txt
-    -1000Facials - Ella Knox - Ella Knox Blowbang.nfo
-  -1000Facials.18.03.12.Alex.More
-    -1000Facials.18.03.12.Alex.More.XXX.SD.MP4.mp4
-  -1000Facials.20.11.09.Natasha.Starr
-    -1kf.20.11.09.natasha.starr.poor.baby.mp4
-ALS Scan
-  -ALSSxan.20.09.01.Gina.Gerson.and.Tina.Hot
-    -ALSScan.20.09.01.Gina.Gerson.and.Tina.Hot.Spotter.BTS.mp4
-    -ALSScan.20.09.01.Gina.Gerson.and.Tina.Hot.Spotter.BTS.nfo
-BANG
-  -I.Dont.Wanna.Be.Fingered.Gimme.Your.Fist
-    -I.Dont.Wanna.Be.Fingered.Gimme.Your.Fist.mp4
-```
-
-Will become:
-
-```
-1000 Facials
-  -1000Facials - Ella Knox - Ella Knox Blowbang.mp4
-  -1000Facials.18.03.12.Alex.More.XXX.SD.MP4.mp4
-  -1kf.20.11.09.natasha.starr.poor.baby.mp4
-ALS Scan
-  -ALSScan.20.09.01.Gina.Gerson.and.Tina.Hot.Spotter.BTS.mp4
-BANG
-  -I.Dont.Wanna.Be.Fingered.Gimme.Your.Fist.mp4
-```
-
-This will make it easier to keep an overview of your content, and for organizer software to manage your video clips.
 
 
 ## ARGUMENTS:
@@ -62,11 +19,23 @@ This will make it easier to keep an overview of your content, and for organizer 
 ## EXAMPLE:
 
 
-flatten -s -v -d /folder/xxxclips
+`flatten -s -v -d /folder/xxxclips`
 
 (will simulate traversal of /folder/xxxclips and print verbose output.
 
 
-Some functionality not yet done, the program always runs in verbose mode currently, and verbosity will print out a lot of text.
+Some functionality not yet done, verbosity will print out a huge load of text.
 
-The utility prints statistics about what it did when it is finished.
+The utility prints statistics when done.
+
+### Calling from Python application
+
+Send the command line parameters to the utility's main function as a list. Like:
+
+```
+import flatten
+x = flatten.main(['-s', '-v', '-d', '/folder/xxxclips'])
+```
+
+Remember, if you're running on Windows, you must send the directory escaped (with double-backslashes).
+
--- flatten.py
+++ flatten.py
@@ -1,28 +1,40 @@
+#!/usr/bin/python
+
 import os, sys, getopt
 from pathlib import Path
 import json
 import shutil
 
-def getConfig() -> dict:
-# This function reads the trash file extensions from JSON
+def getConfig () -> dict:
+    # This function reads the trash file and allowed video file extensions from a JSON file
+
     thisdir = os.path.dirname(__file__)
-    with open(os.path.abspath(os.path.join(thisdir,'flatten-config.json')), 'r') as config:
+    with open(os.path.abspath(os.path.join(thisdir, 'flatten-config.json')), 'r') as config:
         data = config.read()
 
-    settings = json.loads(data)    
+    settings = json.loads(data)
     configuration = settings
     return configuration
 
-def printDirectoryFiles(directory: str, root: str):
+
+def printDirectoryFiles (directory: str, root: str):
+    """
+    This function works with the individual files and directories and decides, based on settings,
+    what is going to happen to them
+
+    :param directory:
+    :param root:
+    :return:
+    """
     global removefile, movefile, removepath, pathnotdel, simulate, verbose, forcermdir
     rootdir = root
     extensions = getConfig()
-    
-    for filename in os.listdir(directory):  
-        full_path=os.path.abspath(os.path.join(directory, filename))
+
+    for filename in os.listdir(directory):
+        full_path = os.path.abspath(os.path.join(directory, filename))
         if not os.path.isdir(full_path):
             for i in extensions['remove-files']:
-                if Path(full_path).suffix == "."+i:
+                if Path(full_path).suffix == "." + i:
                     if not simulate:
                         try:
                             print(f"--- Removing file:\n    {full_path}")
@@ -33,21 +45,22 @@ def printDirectoryFiles(directory: str, root: str):
                     else:
                         print(f"--- Would remove file:\n    {full_path}")
                     removefile += 1
-                    
+
             for v in extensions['files-to-move']:
-                if Path(full_path).suffix == "."+v:
+                if Path(full_path).suffix == "." + v:
                     if not simulate:
                         try:
-                            print(f">>> Moving file:\n    {full_path}\n    to {os.path.join(rootdir,os.path.basename(full_path))}")
-                            shutil.move(full_path, os.path.join(rootdir,os.path.basename(full_path)))
+                            print(
+                                f">>> Moving file:\n    {full_path}\n    to {os.path.join(rootdir, os.path.basename(full_path))}")
+                            shutil.move(full_path, os.path.join(rootdir, os.path.basename(full_path)))
                         except OSError as exc:
                             print(f"!!! Error: {exc}")
-                            pass                   
+                            pass
                     else:
-                        print(f">>> Would move file:\n    {full_path}\n    to {os.path.join(rootdir,os.path.basename(full_path))}")
+                        print(
+                            f">>> Would move file:\n    {full_path}\n    to {os.path.join(rootdir, os.path.basename(full_path))}")
                     movefile += 1
         else:
-            #if not os.listdir(full_path): (would only remove if dir is empty, now handled differently)
             if not simulate:
                 try:
                     print(f"--- Removing dir:\n    {full_path} (force: {forcermdir})")
@@ -65,114 +78,136 @@ def printDirectoryFiles(directory: str, root: str):
                 removepath += 1
 
 
-def checkFolders(directory: str):
-# This function checks all root folders within the selected folder.
-# These are the folders that video scene files should be moved to.
-#
-# The root variable makes sure that any video scenes recursively are moved to the main folder 
-# (the one directly below the defined starting directory).
+def checkFolders (directory: str):
+    """
+    This function checks all root folders within the selected folder.
+    These are the folders that video scene files should be moved to.
+    The root variable makes sure that any video scenes recursively are moved to the main folder
+    (the one directly below the defined starting directory).
+
+    :param directory:
+    :return:
+    """
+
     global verbose
     dir_list = next(os.walk(directory))[1]
 
-    for dir in dir_list:           
+    for dir in dir_list:
         print(f"\n*** Now in a new parent directory -> {dir}") if verbose else print(f"\n*** Now in directory -> {dir}")
-        root = os.path.abspath(os.path.join(directory,dir))
-        checkSubFolders(os.path.abspath(os.path.join(directory,dir)), root) 
+        root = os.path.abspath(os.path.join(directory, dir))
+        checkSubFolders(os.path.abspath(os.path.join(directory, dir)), root)
 
-    #printDirectoryFiles(directory)       
+        # printDirectoryFiles(directory)
 
 
-def checkSubFolders(directory: str, root: str):
-# This function checks all folders within the calling root folder (recursively)
-# To ensure that the files are moved to the first subfolder (read above), the original root is passed on.
+def checkSubFolders (directory: str, root: str):
+    """
+    This function checks all folders within the calling root folder (recursively)
+    To ensure that the files are moved to the first subfolder (read above), the original root is passed on.
+
+    :param directory:
+    :param root:
+    :return:
+    """
 
     dir_list = next(os.walk(directory))[1]
-    
-    for dir in dir_list:           
+
+    for dir in dir_list:
         if verbose: print(f"\n*** Now in subdirectory -> {dir}")
-        checkSubFolders(directory +"/"+ dir, root) 
-        
-    printDirectoryFiles(directory, root)       
+        checkSubFolders(directory + "/" + dir, root)
 
-def printhelp():
-    print ("FLATTEN 0.6")
-    print ("===========")
-    print ("")
-    print ("A utility to flatten adult video scene directories")
-    print ("Will work from an input directory and move all files below the parent subfolders")
-    print ("to the parent subfolder, and delete any trash files (configurable in flatten-config.json)")
-    print ("")
-    print ("ARGUMENTS:")
-    print ("----------")
-    print ("")
-    print ("-h, --help:        Prints this help text")
-    print ("-s, --simulate:    Simulates the operation, nothing is deleted or moved")
-    print ("-v, --verbose:     Verbosity on (lots of text will be printed, pass it to a log by ending")
-    print ("                   the command with \" > log.txt\"")
-    print ("-f, --force-rmdir: Removes subdirectories below the various main directories, even if not empty")
-    print ("                   (there may be additional residual files besides those in the trash list)")
-    print ("-d <dir>,")
-    print ("--directory <dir>: Pass the working directory to the program with this argument.")
-    print ("")
-    print ("EXAMPLE:")
-    print ("--------")
-    print ("")
-    print ("flatten -s -v -d /folder/xxxclips")
-    print ("(will simulate traversal of /folder/xxxclips and print verbose output.")
-    
-    
-removefile = 0
-movefile = 0
-removepath = 0
-pathnotdel = 0
-forcermdir = False
-verbose = False
-simulate = False
-path = ""
-argument_list = sys.argv[1:]
+    printDirectoryFiles(directory, root)
 
-short_options = "hd:fvs"
-long_options = ["help", "directory=", "force-rmdir", "verbose", "simulate"]
 
-try:
-    arguments, values = getopt.getopt(argument_list, short_options, long_options)
-except getopt.error:
-    # Output error, and return with an error code
-    print("ERROR: Wrong arguments.\nUse -h or --help for help.")
-    sys.exit(2)
+def printhelp ():
+    # Prints a help text
 
-if not arguments:
-    printhelp()
-    sys.exit(2)
-    
-for current_argument, current_value in arguments:
-    if current_argument in ("-v", "--verbose"):
-        print ("Enabling verbose mode")
-        verbose = True
-    if current_argument in ("-s", "--simulate"):
-        print("Simulating operation")
-        simulate = True
-    if current_argument in ("-f", "--force-rmdir"):
-        print("Forcing removal of subdirectories")
-        forcermdir = True
-    elif current_argument in ("-h", "--help"):
-        printhelp()     
+    print("FLATTEN 0.6")
+    print("===========")
+    print("")
+    print("A utility to flatten adult video scene directories")
+    print("Will work from an input directory and move all files below the parent subfolders")
+    print("to the parent subfolder, and delete any trash files (configurable in flatten-config.json)")
+    print("")
+    print("ARGUMENTS:")
+    print("----------")
+    print("")
+    print("-h, --help:        Prints this help text")
+    print("-s, --simulate:    Simulates the operation, nothing is deleted or moved")
+    print("-v, --verbose:     Verbosity on (lots of text will be printed, pass it to a log by ending")
+    print("                   the command with \" > log.txt\"")
+    print("-f, --force-rmdir: Removes subdirectories below the various main directories, even if not empty")
+    print("                   (there may be additional residual files besides those in the trash list)")
+    print("-d <dir>,")
+    print("--directory <dir>: Pass the working directory to the program with this argument.")
+    print("")
+    print("EXAMPLE:")
+    print("--------")
+    print("")
+    print("flatten -s -v -d /folder/xxxclips")
+    print("(will simulate traversal of /folder/xxxclips and print verbose output.")
+
+def main(args: list):
+    # This is the main program
+
+    global removefile, movefile, removepath, pathnotdel, simulate, verbose, forcermdir
+    removefile = 0
+    movefile = 0
+    removepath = 0
+    pathnotdel = 0
+    verbose = False
+    simulate = False
+    path = ""
+    argument_list = args
+
+    short_options = "hd:fvs"
+    long_options = ["help", "directory=", "force-rmdir", "verbose", "simulate"]
+
+    try:
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    except getopt.error:
+        # Output error, and return with an error code
+        print("ERROR: Wrong arguments.\nUse -h or --help for help.")
         sys.exit(2)
-    elif current_argument in ("-d", "--directory"):
-        path = current_value
-        print (f"Working from {path}")
-    
 
-checkFolders(path)
-print("")
-if simulate:
-    print(f"If you had not simulated this operation, Flatten would have done this:")
-    print(f"- Moved {movefile} video scenes.")
-    print(f"- Removed {removefile} trash files and {removepath} subfolders (if possible).")
-    #print(f"- Not able to remove {pathnotdel} subfolders.")
-else:
-    print(f"Flatten is now done flattening {path}.")
-    print(f"This was completed:")
-    print(f"- Moved {movefile} video scenes.")
-    print(f"- Removed {removefile} trash files and {removepath} subfolders.")
-    print(f"- Not able to remove {pathnotdel} subfolders.")
+    if not arguments:
+        printhelp()
+        sys.exit(2)
+
+    for current_argument, current_value in arguments:
+        if current_argument in ("-v", "--verbose"):
+            print("Enabling verbose mode")
+            verbose = True
+        if current_argument in ("-s", "--simulate"):
+            print("Simulating operation")
+            simulate = True
+        if current_argument in ("-f", "--force-rmdir"):
+            print("Forcing removal of subdirectories")
+            forcermdir = True
+        elif current_argument in ("-h", "--help"):
+            printhelp()
+            sys.exit(2)
+        elif current_argument in ("-d", "--directory"):
+            path = current_value
+            print(f"Working from {path}")
+
+
+    checkFolders(path)
+    print("")
+    if simulate:
+        print(f"If you had not simulated this operation, Flatten would have:")
+        print(f"- Moved {movefile} video scenes.")
+        print(f"- Removed {removefile} trash files and {removepath} subfolders (if possible).")
+        #print(f"- Not able to remove {pathnotdel} subfolders.")
+    else:
+        print(f"Flatten is now done flattening {path}.")
+        print(f"This was completed:")
+        print(f"- Moved {movefile} video scenes.")
+        print(f"- Removed {removefile} trash files and {removepath} subfolders.")
+        print(f"- Not able to remove {pathnotdel} subfolders.")
+
+
+if __name__ == "__main__":
+    print(sys.argv[1:])
+    quit()
+    main(sys.argv[1:])
